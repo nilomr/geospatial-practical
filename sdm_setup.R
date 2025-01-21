77bd7a639d1067ac3dc7a2a78de1c588bd9c6511
nilomr <nilomerinorecalde@gmail.com>
2025-01-21 18:58:44 +0000

    Add renv configuration and initial package setup for R environment

---
+++ sdm_setup.R
@@ -0,0 +1,8 @@
+# First time you run these lines and install the packages. 
+# You only need to run this once.
+install.packages("dismo") 
+install.packages("rworldmap")
+install.packages("sf")
+install.packages("geodata")
+install.packages("here")
+install.packages("tibble") # Undeclared dependency of sf::read_sf(). Amateurs.
